7d58645cac09320dba2b54ec2e297ae42971577b
Yuma Gunawan <yumaanugrahviryagunawan@bangunindo.com>
2024-01-21 13:34:16 +0800

    add required default val on note schema

--- app/api/note/schemas.py
+++ app/api/note/schemas.py
@@ -2,15 +2,15 @@ from pydantic import BaseModel, Field, EmailStr
 from api.base.base_schemas import BaseResponse, PaginationParams
 
 class AddNoteRequest(BaseModel):
-    title: str = Field(min_length=1, max_length=255)
-    content: str = Field(min_length=1, max_length=500)
+    title: str = Field(..., min_length=1, max_length=255)
+    content: str = Field(..., min_length=1, max_length=500)
 
 class AddNoteResponse(BaseResponse):
     data: dict | None
 
 class UpdateNoteRequest(BaseModel):
-    title: str = Field(min_length=1, max_length=255)
-    content: str = Field(min_length=1, max_length=500)
+    title: str = Field(..., min_length=1, max_length=255)
+    content: str = Field(..., min_length=1, max_length=500)
 
 class UpdateNoteResponse(BaseResponse):
     data: dict | None
